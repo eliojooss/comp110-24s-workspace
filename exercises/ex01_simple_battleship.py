90bf142955af700810f270e65cad29d86f4cc881
Elio Jooss <ejooss@unc.edu>
2024-01-21 20:02:24 -0500

    first turn in version

--- exercises/ex01_simple_battleship.py
+++ exercises/ex01_simple_battleship.py
@@ -2,34 +2,55 @@
 
 __author__ = "730484781"
 
-user_input1: str = input("pick a secret boat location between 1 and 4: ")
+BLUE_BOX: str = "\U0001F7E6"
+RED_BOX: str = "\U0001F7E5"
+WHITE_BOX: str = "\U00002B1C"
 
+user_input1: str = input("pick a secret boat location between 1 and 4: ")
 user_number1: int = int(user_input1)
+
 if user_number1 < 1:
     print("Error!",user_number1,"too low!")
+    exit()
 if user_number1 > 4:
     print("Error!",user_number1,"too high!")
-
-user_input2: str = input("pick a secret boat location between 1 and 4: ")
-
-user_number2: int = int(user_input2)
-if user_number2 < 1:
-    print("Error!",user_number2,"too low!")
-if user_number2 > 4:
-    print("Error!",user_number2,"too high!")
+    exit()
 
 user_guess1: str = input("guess a number between 1 and 4: ")
-
 user_guessint1: int = int(user_guess1)
-if user_guessint1 == user_number2:
-    print("Correct! You hit the ship.")
+if user_guessint1 < 1:
+    print("Error!",user_number1,"too low!")
+    exit()
+if user_guessint1 > 4:
+    print("Error!",user_number1,"too high!")
+    exit()
+
+final_display: str = (BLUE_BOX + BLUE_BOX + BLUE_BOX + BLUE_BOX )
+
+if user_guessint1 == user_number1:
+    user_guess_color: str = (WHITE_BOX)
+    if user_guessint1 == 1:
+        final_display = (user_guess_color + BLUE_BOX + BLUE_BOX + BLUE_BOX)
+    if user_guessint1 == 2:
+        final_display = (BLUE_BOX + user_guess_color + BLUE_BOX + BLUE_BOX)
+    if user_guessint1 == 3:
+        final_display = (BLUE_BOX + BLUE_BOX + user_guess_color + BLUE_BOX)
+    if user_guessint1 == 4:
+        final_display = (BLUE_BOX + BLUE_BOX + BLUE_BOX + user_guess_color)
+    print(final_display)
+    print("Correct! You hit the ship.")    
 else:
+    user_guess_color: str = (RED_BOX)
+    if user_guessint1 == 1:
+        final_display = (user_guess_color + BLUE_BOX + BLUE_BOX + BLUE_BOX)
+    if user_guessint1 == 2:
+        final_display = (BLUE_BOX + user_guess_color + BLUE_BOX + BLUE_BOX)
+    if user_guessint1 == 3:
+        final_display = (BLUE_BOX + BLUE_BOX + user_guess_color + BLUE_BOX)
+    if user_guessint1 == 4:
+        final_display = (BLUE_BOX + BLUE_BOX + BLUE_BOX + user_guess_color)
+    print(final_display)
     print("Incorrect! You missed the ship.")
 
-user_guess2: str = input("guess a number between 1 and 4: ")
 
-user_guessint2: int = int(user_guess2)
-if user_guessint2 == user_number1:
-    print("Correct! You hit the ship.")
-else:
-    print("Incorrect! You missed the ship.")
+
