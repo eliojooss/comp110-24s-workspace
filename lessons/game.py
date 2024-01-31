e7ce5f1ef94472d2a44053f9b02d68796701981a
Elio Jooss <ejooss@unc.edu>
2024-01-31 15:16:52 -0500

    inclass

---
+++ lessons/game.py
@@ -0,0 +1,17 @@
+"""Number guessing game."""
+from random import randint
+
+SECRET: int = randint(1,10)
+correct: bool = False
+
+while not correct: #same as saying while correct == False
+    guess: int = int(input("Guess a number: "))
+    if guess == SECRET:
+        print(f"You got it right! {guess} is the secret number!")
+        correct = True
+    else:
+        if guess > 10:
+            print("Your guess is to low!")
+        if guess < 1:
+            print("Your guess is to low!")
+        print("Try again!")
