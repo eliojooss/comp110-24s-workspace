480fc2e469cc7ca667f3341ff65a154819614be8
Elio Jooss <ejooss@unc.edu>
2024-02-02 14:02:22 -0500

    final #1

--- exercises/ex02_one_shot_battleship.py
+++ exercises/ex02_one_shot_battleship.py
@@ -1,7 +1,7 @@
 """EX02 - One Shot Battleship - The next step in Battleship """
 
 __author__ = "730484781"
-
+#following lines of code establish the grid size, secret row and column, the box emojis, and the boolean variables for repeated inputs if the guess is out of range
 GRID: int = 4
 SECRET_ROW: int = 3
 SECRET_COLUMN: int = 2 
@@ -13,6 +13,7 @@ WHITE_BOX: str = "\U00002B1C"
 run_row: bool = True
 run_column: bool = True
 
+#the following variables and while loops allow for users to guess a row and column and will let them guess repeatedly until their guess is within the grid
 user_row: int = int(input("Guess a row: "))
 while run_row == True:
 
@@ -29,6 +30,7 @@ while run_column == True:
     else:
         run_column = False
 
+#the following code sets the result_box based on the guess and constructs the grid using while loops to check if the guess equals the row or column
 result_box: str = BLUE_BOX
 if SECRET_ROW == user_row and SECRET_COLUMN == user_column:
     result_box: str = RED_BOX
@@ -55,13 +57,12 @@ while row_counter <= GRID:
     row_counter += 1
     print(row_string)
 
-
-    
-
-
-
-
+#this final section of code simply checks if the column, row, or both are correct and outputs the accurate text
 if SECRET_ROW == user_row and SECRET_COLUMN == user_column:
     print("Hit!")
+elif SECRET_ROW == user_row:
+    print("Close! Correct row, wrong column.")
+elif SECRET_COLUMN == user_column:
+    print("Close! Correct column, wrong row.")
 else:
     print("Miss!")
