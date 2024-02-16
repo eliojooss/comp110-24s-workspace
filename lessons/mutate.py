485c64780b1347d40200152ec7eb31c5c0fafd71
Elio Jooss <ejooss@unc.edu>
2024-02-16 16:36:16 -0500

    attempt2

--- lessons/mutate.py
+++ lessons/mutate.py
@@ -2,11 +2,13 @@
 
 __author__ = "730484781"
 
-def manual_append(list: list[int], num: int):
+def manual_append(list: list[int], num: int) -> None:
+    """append a list and an int"""
     list.append(num)
 
-def double(list:list[int]):
+def double(list: list[int]) -> None:
+    """double a list"""
     counter: int = 0
     while counter < len(list):
         list[counter] *= 2
-        counter +=1
+        counter += 1
