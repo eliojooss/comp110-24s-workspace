66d64c37594836ceba18cdd0771c206c3967bb8b
Elio Jooss <ejooss@unc.edu>
2024-03-27 11:06:28 -0400

    practicing

--- lessons/quiz3.py
+++ lessons/quiz3.py
@@ -1,25 +1,4 @@
-def odd_even(list: list[int]) -> list[int]:
-    new_list: list[int] = []
-    for i in range(0, len(list)):
-        if i % 2 == 0:
-            if list[i] % 2 == 1:
-                new_list.append(list[i])
-    return new_list
-
-def short_words(list: list[str]) -> list[str]:
-    return_list: list[str] = []
-    for i in list:
-        if len(i) < 5:
-            return_list.append(i)
-        else:
-            print(f"{i} is too long!")
-    return return_list
-
-def double(x: int) -> int:
-    return x * 2
-
-def double_display(y: int):
-    print(y * 2)
-
-double_display(2)
-    
+nums = [1,2,3,4,5]
+for i in range(len(nums)):
+    nums[i] = nums[i]*2
+print(nums)
