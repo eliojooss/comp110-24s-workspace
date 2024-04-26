0f7fc6cfb629b759aac097f2e4831b5159446463
Elio Jooss <ejooss@unc.edu>
2024-04-26 16:01:42 -0500

    cq09

--- exercises/ex10/linked_list.py
+++ exercises/ex10/linked_list.py
@@ -1,5 +1,41 @@
 """Utility functions for working with Linked Lists."""
 
-from __future import annotations
+from __future__ import annotations
 
-__author__ = 
+__author__ = "730484781"
+
+
+class Node: 
+    """An item in a singly-linked list."""
+    data: int
+    next: Node | None
+
+    def __init__(self, data: int, next: Node | None):
+        """Construct a singly linked list. Use None for 2nd argument if tail."""
+        self.data = data
+        self.next = next
+
+    def __str__(self) -> str: 
+        """Produce a string visualization of the linked list."""
+        if self.next is None:
+            return f"{self.data} -> None"
+        else: 
+            return f"{self.data} -> {self.next}"
+        
+    def head(self) -> int:
+        """Return head."""
+        return self.data
+    
+    def tail(self) -> Node | None:
+        """Return tail."""
+        return self.next
+    
+    def last(self) -> int:
+        """Return last."""
+        last = self
+        while last.next is not None:
+            last = last.next
+        return last.data
+
+def value_at(head: Node | None, index: int) -> int:
+    
